18bf4e46b9fe04618065968bc3442d8494332924
Shiraishi Yukihiro <s.yukihiro.723@gmail.com>
2023-05-23 09:52:23 +0900

    powershellコマンドを修正

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 pyinstaller .\example.py --onefile
-Compress-Archive -Path .\dist\example.exe, .\manual.txt, .\サンプル.tiff, .\settings.json -DestinationPath .\archive.zip
+powershell -Command "Compress-Archive -Path .\dist\example.exe, .\manual.txt, .\サンプル.tiff, .\settings.json -DestinationPath .\archive.zip"
