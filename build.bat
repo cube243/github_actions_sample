f313f856766d4dc10bff79657908cb2652590d88
Shiraishi Yukihiro <s.yukihiro.723@gmail.com>
2023-05-25 15:15:09 +0900

    エンコーディングをutf-8に戻す

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 pyinstaller .\example.py --onefile
-powershell -Command "Compress-Archive -Path .\dist\example.exe, .\manual.txt, .\ƒTƒ“ƒvƒ‹.tiff, .\settings.json -DestinationPath .\archive.zip"
+powershell -Command "Compress-Archive -Path .\dist\example.exe, .\manual.txt, .\ã‚µãƒ³ãƒ—ãƒ«.tiff, .\settings.json -DestinationPath .\archive.zip"
