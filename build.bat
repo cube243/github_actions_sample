3dc2874c80d1894b1d9e965fe433709f30a5d1a3
Shiraishi Yukihiro <s.yukihiro.723@gmail.com>
2023-05-25 15:38:59 +0900

    windowsのローカル環境で実行時にエンコーディングをutf-8に変更

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 @echo off
+chcp 65001
 pyinstaller .\example.py --onefile
 powershell -Command "Compress-Archive -Path .\dist\example.exe, .\manual.txt, .\サンプル.tiff, .\settings.json -DestinationPath .\archive.zip"
